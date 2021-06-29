f5c35b54cd46f53abd7c390905122cbf0400e521
jackcornblum <jcornblum@gmail.com>
2021-06-29 14:55:30 -0400

    commit tree 2f2656eb7f6fcf314731076ecd6b5df81b3b30c2

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,44 @@
 # code here!
+require "pry"
+
+class School
+    attr_accessor :roster, :name
+
+    def initialize(nameArg)
+        @name = nameArg
+        @roster = {}
+    end
+
+    def add_student(student, grade)
+        @roster[grade] ||= []
+        # binding.pry
+        @roster[grade] << student
+        # binding.pry
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        sorted = {}
+        @roster.each do |grade, student|
+            sorted[grade] = student.sort
+        end
+        sorted
+    end
+end
+
+school = School.new("Bayside High School")
+school.add_student("Jack", 9)
+school.add_student("Ben", 9)
+school.add_student("Larry", 9)
+school.add_student("Alex", 9)
+
+# school.send(:add_student=, "Jack", 9)
+# school.send(:add_student=, "Ben", 9)
+
+binding.pry
+
+
+0
